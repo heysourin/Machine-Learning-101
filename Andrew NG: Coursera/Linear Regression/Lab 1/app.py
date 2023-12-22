56cf233c4faf301c0c9eac69f90d10d848e5c528
Sourin <98945276+heysourin@users.noreply.github.com>
2023-12-22 13:07:38 +0530

    Update app.py

--- Andrew NG: Coursera/Linear Regression/Lab 1/app.py
+++ Andrew NG: Coursera/Linear Regression/Lab 1/app.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 # x_train is the input variable (size in 1000 square feet)
 # y_train is the target (price in 1000s of dollars)
-x_train = np.array([1.0, 2.0])
+x_train = np.array([1.0, 2.0,])
 y_train = np.array([300.0, 500.0])
 # print(f"x_train = {x_train}")
 # print(f"y_train = {y_train}")
@@ -20,9 +20,9 @@ print(f"Number of training examples is: {m}")
 
 # i = 1  # Change this to 1 to see (x^1, y^1)
 for i in range(m):
-  x_i = x_train[i]
-  y_i = y_train[i]
-  print(f"(x^({i}), y^({i})) = ({x_i}, {y_i})")
+    x_i = x_train[i]
+    y_i = y_train[i]
+    print(f"(x^({i}), y^({i})) = ({x_i}, {y_i})")
 
 # Plot the data points
 plt.scatter(x_train, y_train, marker='x', c='r')
@@ -37,12 +37,14 @@ plt.xlabel('Size (1000 sqft)')
 # ----------------------------------------------------------------#
 # 𝑓𝑤,𝑏(𝑥(𝑖))=𝑤𝑥(𝑖)+𝑏
 
-w = 100
+print("Geometric intuition:")
+w = 100  # given 100 initially
 b = 100
 print(f"w: {w}")
 print(f"b: {b}")
 
 
+# this function just draws the straightline
 def compute_model_output(x, w, b):
     """
     Computes the prediction of a linear model
@@ -52,8 +54,8 @@ def compute_model_output(x, w, b):
     Returns
       f_wb (ndarray (m,)): model prediction
     """
-    m = x.shape[0]
-    f_wb = np.zeros(m)
+    m = x.shape[0]  # total training models
+    f_wb = np.zeros(m)  # creating array with m elements (all values = 0)
     for i in range(m):
         f_wb[i] = w * x[i] + b
 
@@ -61,11 +63,12 @@ def compute_model_output(x, w, b):
 
 
 temp_f_wb = compute_model_output(x_train, w, b)
+print("Values of temp_f_wb: ", temp_f_wb)
 # Plot our model prediction
-plt.plot(x_train, temp_f_wb, c='b',label='Our Prediction')
+plt.plot(x_train, temp_f_wb, c='b', label='Our Prediction')
 
 # Plot the data points
-plt.scatter(x_train, y_train, marker='x', c='r',label='Actual Values')
+plt.scatter(x_train, y_train, marker='x', c='r', label='Actual Values')
 
 # Set the title
 plt.title("Housing Prices")
@@ -74,11 +77,21 @@ plt.ylabel('Price (in 1000s of dollars)')
 # Set the x-axis label
 plt.xlabel('Size (1000 sqft)')
 plt.legend()
-plt.show()
+# plt.show()
 
-w = 200                         
-b = 100    
+print("Mathematical intuition:")
+w = 200
+b = 100
 x_i = 1.2
-cost_1200sqft = w * x_i + b    
+cost_1200sqft = w * x_i + b
 
 print(f"${cost_1200sqft:.0f} thousand dollars")
+
+
+"""
+In linear regression, the line that connects two existing points is indeed a valid representation of a linear relationship between those points. However, in the context of machine learning, we often deal with multiple data points and we aim to find a line (or hyperplane in higher dimensions) that best fits all these points. This is done by minimizing the sum of squared residuals (the differences between the observed and predicted values).
+
+f_wb is a point that is y-cap, or predicted output.
+
+The value of b is an arbitrary choice in this example. In a real-world scenario, b would be determined through the training process of the machine learning model, where the model learns the best values of w and b that minimize the error of the model 
+"""
